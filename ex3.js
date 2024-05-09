b9cb10533249f5babf169b535c0d36ee25d96e5e
ISABELLE VICTORIA DA SILVA COIMBRA <62060930340@info.local>
2024-05-09 10:16:52 -0300

    feat:ex3.js

---
+++ ex3.js
@@ -0,0 +1,9 @@
+//Exercício 3: Ímpar ou par
+//Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
+
+let numero = parseFloat(prompt("Escreva qualquer numero:"))
+
+if (numero % 2 == 0) {
+    console.log ("O número Par")
+}
+ else (console.log ("O número é Impar"))
