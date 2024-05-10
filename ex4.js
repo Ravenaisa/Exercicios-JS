b794eaa3f33637c5b7d393786bd97b9188fe44ec
ISABELLE VICTORIA DA SILVA COIMBRA <62060930340@info.local>
2024-05-10 08:26:21 -0300

    ex4.js

--- ex4.js
+++ ex4.js
@@ -3,13 +3,24 @@
 //notas e a mensagem de APROVADO para média superior ou igual a 7,0 RECUPERAÇÃO para
 //notas entre 5.0 e 7,0 ou a mensagem de REPROVADO para média inferior a 5,0.
 
-let nota1 = parseFloat(promp("Digite a primeira nota"))
+let nota1 = parseFloat(prompt("Digite a primeira nota: "))
 
-let nota2 = parseFloat(promp("Digite a segunda nota"))
+let nota2 = parseFloat(prompt("Digite a segunda nota: "))
 
-let nota3 = parseFloat(promp("Digite a terceira nota"))
+let nota3 = parseFloat(prompt("Digite a terceira nota: "))
 
-let nota4 = parseFloat(promp("Digite a quarta nota"))
+let nota4 = parseFloat(prompt("Digite a quarta nota: "))
 
-let notafinal = ((nota1+nota2+nota3+nota4)/4)
+let notafinal = (nota1 + nota2 + nota3 + nota4) /4
+if (notafinal >= 7.0) {
+    console.log("Aprovado")
+}
+
+else if (notafinal <=5.0) {
+    console.log("Reprovado")
+}
+
+else {
+    console.log("Recuperação")
+}
 
