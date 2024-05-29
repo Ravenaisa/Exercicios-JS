e0bb2791c84713111d9c38330edb71ed9d45d5a3
ISABELLE VICTORIA DA SILVA COIMBRA <62060930340@info.local>
2024-05-29 10:40:06 -0300

    exercicios finalizados

--- if-else/ex2.js
+++ if-else/ex2.js
@@ -4,29 +4,40 @@
 //semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
 //2 representa terça-feira e assim por diante.
 
-let numero = prompt("Digite um número de 1 a 7")
+let dia_semana = prompt("Informe um número de 1 a 7: ");
 
+switch (dia_semana) {
 
+    case "1":
+        console.log("Segunda-feira");
+        break;
 
-//números de acordo com a semana 
+    case "2":
+        console.log("Terça-feira");
+        break;
 
-if(numero==1) {
-    console.log ("Segunda-Feira")
-}
+    case "3":
+        console.log("Quarta-feira");
+        break;
 
-if(numero==2) {
-    console.log ("Terça-Feira")
-}
+    case "4":
+        console.log("Quinta-feira");
+        break;
 
-if(numero==3) {
-    console.log ("Quarta-Feira")
-}
+    case "5":
+        console.log("Sexta-feira");
+        break;
 
-if(numero==4) {
-    console.log ("Quinta-Feira")
-}
+    case "6":
+        console.log("Sábado");
+        break;
 
-if(numero==5) {
-    console.log ("Sexta-Feira")
+    case "7":
+        console.log("Domingo");
+        break;
+
+    default:
+        console.log("O número informado não é valido");
+        break;
 }
 
